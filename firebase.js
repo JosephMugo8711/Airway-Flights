13a407feaf2e059fe3d9b197194b83aa1fa85af9
josephwamiti <wamiti8711@gmail.com>
2023-04-30 15:50:10 +0300

    install firebase and navbar

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzJj-fcQQ1A3UkqBiLiC8HiU5iZOIfRIE",
+  authDomain: "booking-flight-20c74.firebaseapp.com",
+  projectId: "booking-flight-20c74",
+  storageBucket: "booking-flight-20c74.appspot.com",
+  messagingSenderId: "41925688990",
+  appId: "1:41925688990:web:55503a72f083d846d8058f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
